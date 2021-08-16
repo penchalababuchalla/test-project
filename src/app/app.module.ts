450909349b579d0ddca29a4917305f1a9b82c567
Penchala Babu <penchalababuchalla@gmail.com>
2021-08-16 13:10:56 +0530

    functionality changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PracCalculatorComponent } from './calculator/prac-calculator.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PracCalculatorComponent
   ],
   imports: [
     BrowserModule,
--- src/app/calculator/prac-calculator.component.ts
+++ src/app/calculator/prac-calculator.component.ts
@@ -1,17 +1,44 @@
 import { Component } from '@angular/core';
 
 @Component({
-	selector:'incriment-decriment',
+	selector:'calculator',
 	templateUrl:'./prac-calculator.component.html',
 	styleUrls: ['./prac-calculator.component.css']
 
 })
 export class PracCalculatorComponent {
 	count=0;
-	incriment(){
-		this.count++
+	input= '';
+	pressNum(num: string){
+		this.input = this.input+num ;
+
 	}
-	decriment(){
-		this.count--
+	result(){
+		// let formula=this.input;
+		this.input =eval(this.input);
+
+	}
+	backspace() {
+    
+      this.input=this.input.substr(0, this.input.length-1)
+    
+  	}
+	clear(){
+		this.input='';
+	}
+
+	factorial(){
+		var n = parseInt(this.input);
+		var i=0;
+		var a=1;
+
+		for (i=n; i>=1; i--){
+
+			a=a*i;
+			console.log('inside')
+			console.log(a)
+		}
+		debugger
+   		this.input = a.toString();
 	}
 }
